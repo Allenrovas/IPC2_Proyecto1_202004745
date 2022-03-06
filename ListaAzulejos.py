fc11ac60773f0be60524bb966a2fe97269c56cff
Allen Román <algirova@gmail.com>
2022-03-06 06:08:31 -0600

    Cuarto commit

--- ListaAzulejos.py
+++ ListaAzulejos.py
@@ -1,6 +1,7 @@
-
 class Azulejo():
-    def __init__(self, rows, columns, color):
+    def __init__(self, rows, columns, color,cantidadrows,cantidadcolumns):
+        self.cantidadrows = cantidadrows
+        self.cantidadcolumns = cantidadcolumns
         self.rows = rows 
         self.columns = columns
         self.color = color
@@ -14,9 +15,8 @@ class ListaAzulejos(object):
         self.contador = 0
 
 
-    def insertar(self, rows, columns, color):
-        nodo = Azulejo(rows, columns, color)
-
+    def insertar(self, rows, columns, color,cantidadrows,cantidadcolumns):
+        nodo = Azulejo(rows, columns, color,cantidadrows,cantidadcolumns)
         if self.cabeza is None:
             self.cabeza = nodo
             self.cola = self.cabeza
@@ -28,12 +28,41 @@ class ListaAzulejos(object):
         self.contador += 1
 
     
-    def recorrer(self):
+    def imprimir(self):
         actual = self.cabeza
-
         while actual:
-            print(actual.rows)
-            print(actual.columns)
-            print(actual.color)
+            print(actual.color," ",actual.columns," ",actual.rows)
             actual = actual.siguiente
+
+    def graficar(self):
+        actual = self.cabeza
+        nodo = 0
+        archivo = open("GraphvizRep.dot", "w")
+        archivo.write('digraph grid {bgcolor="slategrey" label="Reporte Graphviz"')
+        archivo.write('layout=dot \n labelloc = "t"')
+        archivo.write('edge [weigth=1000 style=dashed color=red4 dir = "both" arrowtail="open" arrowhead="open"]\n')
+        archivo.write('rankdir="LR"\n')
+        archivo.write("node[shape=box, color=lightgrey]") 
+        archivo.write("a0 [label=<\n"+"<TABLE><TR>")
+        ContadorC = 0
+        while actual!= None:
+            if ContadorC < actual.cantidadcolumns:
+                if actual.color == "W":
+                    archivo.write('<TD bgcolor="white"></TD>\n')
+                elif actual.color == "B":
+                    archivo.write('<TD bgcolor="black"></TD>\n')
+                ContadorC += 1
+                actual = actual.siguiente
+            else:
+                ContadorC = 0
+                archivo.write("</TR><TR>")
+
+        archivo.write("</TR></TABLE>>]")
+        archivo.write("}")
+        archivo.close()
+        from os import system
+        from os import system, startfile
+        system('dot.exe -Tpng GraphvizRep.dot -o GraphvizRep.png')
+        system('GraphvizRep.png')
+            
             
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import imp
 from ListaAzulejos import *
 from ListaPatrones import *
 from ListaPisos import *
@@ -32,54 +31,64 @@ def LeerXml():
             if Contador == 4:
                 nodo = Piso(nombre, rows,columns, flip, slide)
                 for subhijo2 in subhijo:
+                    listaazulejos = ListaAzulejos()
                     codigo = subhijo2.attrib['codigo']
                     cadena = subhijo2.text
                     ContadorRows = 0
                     ContadorColumns = 0
-                    for letra in cadena:
-                        if ContadorColumns < columns:
-                            nodo.casillas.insertar(ContadorRows,ContadorColumns,letra)
+                    ContadorAuxiliar = len(cadena)
+                    C = 0
+                    while C < len(cadena):
+                        if ContadorColumns < columns: #0<2;1<2 
+                            listaazulejos.insertar(ContadorRows,ContadorColumns,cadena[C],rows,columns)
                             ContadorColumns+=1
+                            C+=1
                         else:
                             ContadorRows+=1
                             ContadorColumns=0
-                    nodo.patrones.insertar(codigo, cadena)
+                    
+                    nodo.patrones.insertar(codigo, cadena,listaazulejos)
+
                 listapisos.insertar(nodo)
             Contador+=1
     print("Se ha cargo el Archivo XML")
 
-          
+def MenuPatrones():
+    listapisos.recorrermenu()
+    
+
 
 def MenuInicial():
-    SeleccionarUsuario = 0
-    print("         Menu Ventas")
-    print("==============================================")
-    print("Pulse el numero correspondiente a la solicitud")
-    print("")
-    print("1. Leer Archivo.")
-    print("2. Ver un piso en específico")
-    print("3. Ver todos los Pisos")
-    print("4. Salir")
-    print("")
-    SeleccionarUsuario = int(input())
-    if SeleccionarUsuario == 1:
+        SeleccionarUsuario = 0
+        print("         Menu Ventas")
+        print("==============================================")
+        print("Pulse el numero correspondiente a la solicitud")
         print("")
-        LeerXml()
-        MenuInicial()
-    elif SeleccionarUsuario == 2:
+        print("1. Leer Archivo.")
+        print("2. Ver un piso en específico")
+        print("3. Ver todos los Pisos")
+        print("4. Salir")
         print("")
-    elif SeleccionarUsuario == 3:
-        listapisos.ordenar_alfabeticamente()
-        listapisos.recorrer()
-        MenuInicial()
-    elif SeleccionarUsuario ==4:
-        print("Gracias por usar nuestro sistema, buen dia.")
-        exit
-    else:
-        print("")
-        print("Ingrese una entrada valida")
-        print("")
-        MenuInicial()
+        SeleccionarUsuario = int(input())
+        if SeleccionarUsuario == 1:
+            print("")
+            LeerXml()
+            MenuInicial()
+        elif SeleccionarUsuario == 2:
+            MenuPatrones()
+        elif SeleccionarUsuario == 3:
+            listapisos.ordenar_alfabeticamente()
+            listapisos.recorrer()
+            MenuInicial()
+        elif SeleccionarUsuario ==4:
+            print("Gracias por usar nuestro sistema, buen dia.")
+            exit
+        else:
+            print("")
+            print("Ingrese una entrada valida")
+            print("")
+            MenuInicial()
+    
 
 MenuInicial()
 
