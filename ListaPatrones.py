bc5391ff1f2d9b55cbc7cd3382f99b0a177b86ae
Allen Román <algirova@gmail.com>
2022-03-06 22:51:16 -0600

    Octavo commit

--- ListaPatrones.py
+++ ListaPatrones.py
@@ -1,9 +1,15 @@
+from ListaAzulejos import *
+
 class Patrones():
     def __init__(self, codigo, cadena):
         self.codigo = codigo
         self.cadena = cadena
+        self.casillas = ListaAzulejos() #Matriz
         self.siguiente = None
         self.anterior = None
+
+    def agregarCasillas(self, casillas):
+        self.casillas.insertar(casillas)
 
 class ListaPatrones(object):
     def __init__(self):
@@ -12,8 +18,9 @@ class ListaPatrones(object):
         self.contador = 0
 
 
-    def insertar(self,  codigo, cadena):
+    def insertar(self,  codigo, cadena, casillas):
         nodo = Patrones( codigo, cadena)
+        nodo.casillas = casillas
 
         if self.cabeza is None:
             self.cabeza = nodo
@@ -30,6 +37,28 @@ class ListaPatrones(object):
         actual = self.cabeza
 
         while actual:
-            print(" "+actual.codigo)
+            print(actual.codigo)
             actual = actual.siguiente
             
+    def Seleccionar(self):
+        actual = self.cabeza
+        i=1
+        Seleccionar = ""
+        print("======Menu Nuevo Patron======")
+        while actual:
+            print(" ",actual.codigo)
+            i+=1
+            actual = actual.siguiente
+        print(" exit. Regresar al menu principal")
+        SeleccionarPiso = input("Ingrese el nombre del piso a graficar: ")
+        actual = self.cabeza
+        for piso in range(i):
+            if SeleccionarPiso == actual.nombre:
+                ListaAzulejos.graficar
+            elif SeleccionarPiso == "exit":
+               print("Help")
+               #MenuInicial.MenuInicial()
+            else:
+                "Ingrese una entrada válida"
+            piso +=1
+    
